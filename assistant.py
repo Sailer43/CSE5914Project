402e875887afe66abe43dd489398e25a595f76d2
sanosenx86 <mu.138@buckeyemail.osu.edu>
2019-09-21 03:59:15 -0400

    Change the send_input function. Add documentation.

--- assistant.py
+++ assistant.py
@@ -14,18 +14,48 @@ session = service.create_session(
 sessionId = session["session_id"]
 
 
-def send_input(user_in):
+def find_action(text: str):
+    """
+    A support function for send_input
+
+    :param
+    text(str): the text from send_input to get the action required.
+    :return:
+    str: the trimmed message,
+    str: the action requested.
+    """
+    start = text.find("!<+")
+    if start == -1:  # The symbol not found
+        return text, ''
+    return text[:start], text[start + 3:]
+
+
+def send_input(text: str):
+    """
+    Get response from Watson Assistant
+
+    :param
+    text (str): user input
+    :return:
+    str: message from Watson,
+    str: action required,
+    str: intent the user input belongs to
+    """
     response = service.message(
         assistant_id='f2ddf51f-1048-44ad-a4a8-3d5a5289178a',
-        session_id = sessionId,
+        session_id=sessionId,
         input={
             'message_type': 'text',
-            'text': user_in
+            'text': text
         }
     ).get_result()
-    message = response['output']['generic'][0]['text']
-    return message
+
+    message, action = find_action(response['output']['generic'][0]['text'])
+    intent = response['output']['intents'][0]['intent']
+    return message, action, intent
 
 
-send_input('hello')
+##send_input('hello')
+##print(find_action('I will query the weather information for you. !<+WEATHER'))
 
+print(send_input("What' the weather tomorrow?"))
